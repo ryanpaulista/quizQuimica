56fd4e562b5a4e7132bf523aa72cbd243db40668
Ryan Paulista <ryanpaulista2610@gmail.com>
2024-12-08 01:32:24 -0300

    mudanças nas questões

--- js/app.js
+++ js/app.js
@@ -1,13 +1,40 @@
 const botoesErrados = document.getElementsByClassName('errados');
+const correta = document.querySelector('.correta');
+const btnNext = document.querySelector('.btn-next');
+const wrongOptions = document.querySelectorAll('.errados');
+var acertos = 0;
+
+    correta.addEventListener('click', acertou);
 
         // Iterar sobre a coleção de elementos
-        for (let botao of botoesErrados) {
-            botao.addEventListener('click', errada);
-        }
+    for (let botao of botoesErrados) {
+        botao.addEventListener('click', errada);
+    }
 
-        function errada() {
-            // Alterar o estilo do botão clicado
-            this.style.backgroundColor = 'rgb(255, 67, 67)';
-            this.innerText='ERRADA'
-        }
+    function errada() {  
+        
+        // Aplica estilo e altera o conteúdo de cada uma
+        wrongOptions.forEach(option => {
+            option.style.backgroundColor = "lightcoral"; // Muda a cor de fundo
+            option.innerHTML = "ERROU"; // Altera o texto para "ERRADA"
+        });
+
+        correta.onclick=null;
+        correta.style.cursor="not-allowed";
+        correta.style.backgroundColor="lightgreen";
+        btnNext.style.display="block";
+        
+    }
+
+    function acertou(){
+        acertos = acertos + 1;
+
+        wrongOptions.forEach(option => {
+            option.style.backgroundColor = "lightgreen"; // Muda a cor de fundo
+            option.innerHTML = "ACERTOU"; // Altera o texto para "ERRADA"
+        }); 
+
+        btnNext.style.display="block";
+        console.log(acertos);
+    }
         
